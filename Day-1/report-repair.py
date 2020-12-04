7359901bb6d6f010b184d75baef5b5e504eb4b0b
Maxime Kabel <maxime.kabel001@gmail.com>
2020-12-04 16:55:08 +0100

    Day-1 Part-2 Complete

--- Day-1/report-repair.py
+++ Day-1/report-repair.py
@@ -9,4 +9,12 @@ for i in input:
         if i != j and i + j == 2020:
             answer = i * j
 
-print(answer)
+answer_pt_2 = 0
+for m in input:
+    for n in input:
+        for l in input:
+            if m != n != l and m + n + l == 2020:
+                answer_pt_2 = m * n * l
+
+print("Answer Part 1:" + str(answer))
+print("Answer Part 2:" + str(answer_pt_2))
