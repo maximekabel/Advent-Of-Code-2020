deca29a8150a55cd6ed536745d547102252c0f7d
Maxime Kabel <maxime.kabel001@gmail.com>
2020-12-06 01:56:29 +0100

    Day-4 Part-1 Complete

--- Day-4/passport-processing.py
+++ Day-4/passport-processing.py
@@ -13,14 +13,43 @@ for b in input:
     passports.append(dic)
 
 for i in passports:
+    valid = True
+    if not ("byr" in i) or not (1920 <= int(i["byr"]) <= 2002):
+        valid = False
+    if not ("iyr" in i) or not (2010 <= int(i["iyr"]) <= 2020):
+        valid = False
+    if not ("eyr" in i) or not (2020 <= int(i["eyr"]) <= 2030):
+        valid = False
+    if not ("hgt" in i):
+        valid = False
+    if "hgt" in i:
+        if i["hgt"].endswith("cm"):
+            if not (150 <= int(i["hgt"][:-2]) <= 193):
+                valid = False
+        elif i["hgt"].endswith("in"):
+            if not (59 <= int(i["hgt"][:-2]) <= 76):
+                valid = False
+        else:
+            valid = False
     if (
-        ("byr" in i)
-        and ("iyr" in i)
-        and ("eyr" in i)
-        and ("hgt" in i)
-        and ("hcl" in i)
-        and ("ecl" in i)
-        and ("pid" in i)
+        not ("hcl" in i)
+        or (i["hcl"][0] != "#")
+        or any([a not in "0123456789abcdef" for a in i["hcl"][1:]])
     ):
+        valid = False
+    if not ("ecl" in i) or i["ecl"] not in [
+        "amb",
+        "blu",
+        "brn",
+        "gry",
+        "grn",
+        "hzl",
+        "oth",
+    ]:
+        valid = False
+    if not ("pid" in i) or len(i["pid"]) != 9 or not i["pid"].isdigit():
+        valid = False
+
+    if valid:
         valid_passport += 1
 print(valid_passport)
